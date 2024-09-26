553332d97a9c54ac071f9700a77107506fca7c43
Hai Nguyen <hainguyenhuynh0501@gmail.com>
2024-09-26 10:39:23 +0700

    feat: revert

--- api/cli.py
+++ api/cli.py
@@ -1,32 +1,7 @@
-from flask import current_app as app
+from app import app
 from database import db
-from database import Role
-
-# Constants for roles
-ADMIN_ROLE = "Admin"
-USER_ROLE = "User"
 
 
-@app.cli.command("create-roles")
-def create_roles():
-    """
-    Custom command to create default roles (Admin and User).
-    Run this command using 'flask create-roles'
-    """
-    if not Role.query.filter_by(name=ADMIN_ROLE).first():
-        db.session.add(Role(name=ADMIN_ROLE))
-    if not Role.query.filter_by(name=USER_ROLE).first():
-        db.session.add(Role(name=USER_ROLE))
-
-    db.session.commit()
-    print("Roles Admin and User created successfully!")
-
-
-@app.cli.command("init-db")
-def init_db():
-    """
-    Initialize the database and create tables.
-    Run this command using 'flask init-db'
-    """
-    db.create_all()
-    print("Database initialized!")
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
